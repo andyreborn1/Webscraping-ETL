418d3d6f94e2c8ce0723e1c2c7e37b1a48c4c33a
Anderson VInicius Ribeiro <andyreborn@hotmail.com.br>
2021-10-06 11:05:50 -0300

    Altera local de salvamento do arquivo

--- webscraping.py
+++ webscraping.py
@@ -26,8 +26,7 @@ def find_file(url):
 
   filename = pdf_download_link.split('/')[-1]
 
-  download_file(pdf_download_link,filename)
-
+  download_file(pdf_download_link,"data/"+ filename)
 
 if __name__ == "__main__":
   URL = 'https://www.gov.br/ans/pt-br/assuntos/prestadores/padrao-para-troca-de-informacao-de-saude-suplementar-2013-tiss'
